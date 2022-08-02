9ff3e81170b7fd436190a33ea1a210cdb9c12986
Amir <46874214+amirsfp@users.noreply.github.com>
2022-08-02 14:06:04 -0400

    Update multi_armed_bandit.py

--- multi_armed_bandit.py
+++ multi_armed_bandit.py
@@ -2,9 +2,15 @@ import numpy as np
 
 
 class MultiArmedBandit:
-
-    def __init__(self, k_arm=10, eps=0.1, initial=0., 
-    UCB_constant=0.1, sample_averages_flag=False, step_size=0.1):
+    def __init__(
+        self,
+        k_arm=10,
+        eps=0.1,
+        initial=0.0,
+        UCB_constant=0.1,
+        sample_averages_flag=False,
+        step_size=0.1,
+    ):
         self.k = k_arm
         self.q_true = np.random.uniform(low=-3.0, high=3.0, size=(self.k,))
         self.q_star = np.max(self.q_true)
@@ -18,7 +24,7 @@ class MultiArmedBandit:
         self.t = 0
         self.sample_averages_flag = sample_averages_flag
         self.step_size = step_size
-    
+
     def reset(self):
 
         self.t = 0
@@ -32,18 +38,19 @@ class MultiArmedBandit:
 
         if np.random.rand() < self.eps:
             action = np.random.choice(self.indices)
-        
+
         else:
-            upper_confidence_bound = self.q_estimation + \
-                self.c * np.sqrt( np.log(self.t + 1) / (self.action_count + 1e-5) )
+            upper_confidence_bound = self.q_estimation + self.c * np.sqrt(
+                np.log(self.t + 1) / (self.action_count + 1e-5)
+            )
             q_best = np.max(upper_confidence_bound)
             action = np.random.choice(np.where(upper_confidence_bound == q_best)[0])
-        
+
         if self.q_true[action] == self.q_star:
             self.optimal_action_count += 1
-        
+
         return action
-    
+
     def step(self, action):
 
         self.t += 1
@@ -51,14 +58,17 @@ class MultiArmedBandit:
         self.action_count[action] += 1
 
         if self.sample_averages_flag:
-            self.q_estimation[action] = (reward - self.q_estimation[action]) \
-                / self.action_count[action]
-        
+            self.q_estimation[action] = (
+                reward - self.q_estimation[action]
+            ) / self.action_count[action]
+
         else:
-            self.q_estimation[action] = self.step_size * (reward - self.q_estimation[action])
-        
+            self.q_estimation[action] = self.step_size * (
+                reward - self.q_estimation[action]
+            )
+
         return reward
-    
+
     def get_optimal_action_ratio(self):
         if self.optimal_action_count:
             return self.optimal_action_count / self.t
@@ -87,7 +97,6 @@ if __name__ == "__main__":
             if run % 100 == 0 and run != 0:
                 print(run)
         print(b)
-        
+
     mean_rewards = rewards.mean(axis=1)
     mean_best_action_ratios = best_action_ratios.mean(axis=1)
-
